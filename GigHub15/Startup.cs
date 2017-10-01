6b62250d8487666f56be447d3fb7f49ac02df56c
hpainter2002 <hpainter2002@gmail.com>
2017-10-01 11:10:09 -0700

    Add project files.

---
+++ GigHub15/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHub15.Startup))]
+namespace GigHub15
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
