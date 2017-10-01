c1b2e378aeccec7b8043306f3cf72908f51df888
hpainter2002 <hpainter2002@gmail.com>
2017-10-01 14:54:41 -0700

    Created Add a Gig Form

---
+++ GigHub15/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using GigHub15.Models;
+using GigHub15.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub15.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
