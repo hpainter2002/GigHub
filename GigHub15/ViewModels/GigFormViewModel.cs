87c91ea0014a7a1b488d86a34091be6fdd6c2833
SURESCRIPTS\hatim.painter <hatim.painter@surescripts.com>
2017-10-04 17:54:59 -0700

    Implement Validation

--- GigHub15/ViewModels/GigFormViewModel.cs
+++ GigHub15/ViewModels/GigFormViewModel.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using GigHub15.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub15.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { set; get; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime DateTime
+        public DateTime GetDateTime()
         {
-            get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); }
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
     }
 }
